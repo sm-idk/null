bae63e22c409022a6b366b794e61a989a4d87740
sm-idk <43745781+sm-idk@users.noreply.github.com>
2025-09-29 23:57:26 +0200

    Added some nice defaults to the keepass configuration

--- home/modules/keepassxc.nix
+++ home/modules/keepassxc.nix
@@ -1,9 +1,20 @@
 { pkgs, ... }:
 {
-
   home.packages = builtins.attrValues {
     inherit (pkgs)
       keepassxc
       ;
+  };
+
+  programs.keepassxc.settings = {
+    General.MinimizeAfterUnlock = true;
+    Browser.Enabled = true;
+    GUI = {
+      ApplicationTheme = "dark";
+      ColorPasswords = true;
+      MinimizeToTray = true;
+      ShowTrayIcon = true;
+      TrayIconAppearance = "monochrome-light";
+    };
   };
 }
