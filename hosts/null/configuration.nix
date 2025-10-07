9400f9d695823bb21a25d68af20ed8cedfe1a0f5
sm-idk <43745781+sm-idk@users.noreply.github.com>
2025-10-07 23:42:14 +0200

    Change display manager to gdm and set up gnome as fallback

--- hosts/null/configuration.nix
+++ hosts/null/configuration.nix
@@ -26,10 +26,9 @@
     initrd.verbose = false;
   };
 
-  services.gnome.gnome-keyring.enable = true;
-  services.colord.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   services.scx.enable = true;
 
   # Hardware
