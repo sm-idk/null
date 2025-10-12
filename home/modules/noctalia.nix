c234c053f357ebfad7236ca80a38ff5b523852ad
sm-idk <43745781+sm-idk@users.noreply.github.com>
2025-10-12 22:33:27 +0200

    Update noctalia.nix

--- home/modules/noctalia.nix
+++ home/modules/noctalia.nix
@@ -18,9 +18,50 @@ in
   imports = [ inputs.noctalia.homeModules.default ];
 
   home.packages = [ inputs.noctalia.packages.${pkgs.system}.default ];
-
   programs = {
-    noctalia-shell.enable = true;
+    noctalia-shell = {
+      enable = true;
+      settings = {
+        widgets = {
+          left = [
+            { id = "Workspace"; }
+          ];
+          center = [
+            { id = "Clock"; }
+          ];
+          right = [
+            { id = "MediaMini"; }
+            { id = "Tray"; }
+            { id = "NotificationHistory"; }
+            { id = "Battery"; }
+            { id = "Volume"; }
+            { id = "Brightness"; }
+            { id = "SystemMonitor"; }
+            { id = "ControlCenter"; }
+          ];
+        };
+        location = {
+          name = "Warsaw";
+        };
+        controlCenter = {
+          position = "close_to_bar_button";
+          quickSettingsStyle = "compact";
+          widgets = {
+            quickSettings = [
+              { id = "WiFi"; }
+              { id = "Bluetooth"; }
+              { id = "Notifications"; }
+              { id = "ScreenRecorder"; }
+              { id = "PowerProfile"; }
+              { id = "WallpaperSelector"; }
+            ];
+          };
+        };
+        colorSchemes = {
+          predefinedScheme = "Monochrome";
+        };
+      };
+    };
     niri.settings.binds = with config.lib.niri.actions; {
       "Mod+L".action.spawn = noctalia "lockScreen toggle";
     };
