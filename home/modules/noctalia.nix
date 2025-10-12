26e7a796c2cc22b5369bc6e9163b45d7b01ddc53
sm-idk <43745781+sm-idk@users.noreply.github.com>
2025-10-12 22:55:00 +0200

    Update noctalia.nix

--- home/modules/noctalia.nix
+++ home/modules/noctalia.nix
@@ -22,40 +22,25 @@ in
     noctalia-shell = {
       enable = true;
       settings = {
-        widgets = {
-          left = [
-            { id = "Workspace"; }
-          ];
-          center = [
-            { id = "Clock"; }
-          ];
-          right = [
-            { id = "MediaMini"; }
-            { id = "Tray"; }
-            { id = "NotificationHistory"; }
-            { id = "Battery"; }
-            { id = "Volume"; }
-            { id = "Brightness"; }
-            { id = "SystemMonitor"; }
-            { id = "ControlCenter"; }
-          ];
+        settingsVersion = 15;
+        bar = {
+          widgets = {
+            left = [ { id = "Workspace"; } ];
+            center = [ { id = "Clock"; } ];
+            right = [
+              { id = "MediaMini"; }
+              { id = "Tray"; }
+              { id = "NotificationHistory"; }
+              { id = "Volume"; }
+              { id = "Brightness"; }
+              { id = "Battery"; }
+              { id = "SystemMonitor"; }
+              { id = "ControlCenter"; }
+            ];
+          };
         };
         location = {
           name = "Warsaw";
-        };
-        controlCenter = {
-          position = "close_to_bar_button";
-          quickSettingsStyle = "compact";
-          widgets = {
-            quickSettings = [
-              { id = "WiFi"; }
-              { id = "Bluetooth"; }
-              { id = "Notifications"; }
-              { id = "ScreenRecorder"; }
-              { id = "PowerProfile"; }
-              { id = "WallpaperSelector"; }
-            ];
-          };
         };
         colorSchemes = {
           predefinedScheme = "Monochrome";
