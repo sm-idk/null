20ffb87945321718a6c8c32570aa0ab8c55c1eaa
sm-idk <43745781+sm-idk@users.noreply.github.com>
2025-10-12 22:09:16 +0200

    Add screen locking in niri when noctalia module is enabled

--- home/modules/noctalia.nix
+++ home/modules/noctalia.nix
@@ -1,8 +1,28 @@
-{ inputs, pkgs, ... }:
+{
+  inputs,
+  pkgs,
+  config,
+  ...
+}:
+let
+  noctalia =
+    cmd:
+    [
+      "noctalia-shell"
+      "ipc"
+      "call"
+    ]
+    ++ (pkgs.lib.splitString " " cmd);
+in
 {
   imports = [ inputs.noctalia.homeModules.default ];
 
   home.packages = [ inputs.noctalia.packages.${pkgs.system}.default ];
 
-  programs.noctalia-shell.enable = true;
+  programs = {
+    noctalia-shell.enable = true;
+    niri.settings.binds = with config.lib.niri.actions; {
+      "Mod+L".action.spawn = noctalia "lockScreen toggle";
+    };
+  };
 }
