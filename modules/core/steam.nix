bb5f374dcc256a769819e343a63388c1ed2833a6
sm-idk <43745781+sm-idk@users.noreply.github.com>
2026-02-01 20:27:01 +0100

    TEMP FIX FOR STEAM

--- modules/core/steam.nix
+++ modules/core/steam.nix
@@ -9,52 +9,53 @@
 
   config = lib.mkIf config.nixos.steam.enable {
     hardware.steam-hardware.enable = true;
-    nixpkgs.overlays = [
-      (_: super: {
-        steam = super.steam.override {
-          extraPkgs =
-            steamSuper:
-            (builtins.attrValues {
-              inherit (steamSuper)
-                curl
-                mesa-demos
-                imagemagick
-                keyutils
-                mangohud
-                source-han-sans
-                steamtinkerlaunch # just in case compattools doesn't works
-                vkbasalt
-                vulkan-validation-layers
-                wqy_zenhei
-                yad
-                ;
-              inherit (pkgsUnstable)
-                libgdiplus
-                libkrb5
-                libpng
-                libpulseaudio
-                libvorbis
-                ;
-              inherit (pkgsUnstable)
-                vulkan-caps-viewer
-                vulkan-extension-layer
-                vulkan-headers
-                vulkan-tools
-                ;
-              inherit (steamSuper.xorg)
-                libXcursor
-                libXi
-                libXinerama
-                libXScrnSaver
-                xhost
-                ;
-              inherit (steamSuper.stdenv.cc.cc) lib;
-            });
-        };
-        # for people that want non official bottles
-        bottles = super.bottles.override { removeWarningPopup = true; };
-      })
-    ];
+
+    # nixpkgs.overlays = [
+    #   (_: super: {
+    #     steam = super.steam.override {
+    #       extraPkgs =
+    #         steamSuper:
+    #         (builtins.attrValues {
+    #           inherit (steamSuper)
+    #             curl
+    #             mesa-demos
+    #             imagemagick
+    #             keyutils
+    #             mangohud
+    #             source-han-sans
+    #             steamtinkerlaunch # just in case compattools doesn't works
+    #             vkbasalt
+    #             vulkan-validation-layers
+    #             wqy_zenhei
+    #             yad
+    #             ;
+    #           inherit (pkgsUnstable)
+    #             libgdiplus
+    #             libkrb5
+    #             libpng
+    #             libpulseaudio
+    #             libvorbis
+    #             ;
+    #           inherit (pkgsUnstable)
+    #             vulkan-caps-viewer
+    #             vulkan-extension-layer
+    #             vulkan-headers
+    #             vulkan-tools
+    #             ;
+    #           inherit (steamSuper.xorg)
+    #             libXcursor
+    #             libXi
+    #             libXinerama
+    #             libXScrnSaver
+    #             xhost
+    #             ;
+    #           inherit (steamSuper.stdenv.cc.cc) lib;
+    #         });
+    #     };
+    #     # for people that want non official bottles
+    #     bottles = super.bottles.override { removeWarningPopup = true; };
+    #   })
+    # ];
 
     programs = {
       steam = {
@@ -65,21 +66,22 @@
         # https://github.com/NixOS/nixpkgs/blob/3730d8a308f94996a9ba7c7138ede69c1b9ac4ae/nixos/modules/programs/steam.nix#L12C3-L12C92
         # steam added a proper search for steamcompattools in your ~/.steam so variable should not be needed anymore
         # don't forget to periodically change steam's compatibility layer
-        extraCompatPackages = builtins.attrValues {
-          inherit (pkgsUnstable)
-            # proton-ge-bin
-            steamtinkerlaunch
-            ;
-        };
+        #
+        # extraCompatPackages = builtins.attrValues {
+        #   inherit (pkgsUnstable)
+        #     # proton-ge-bin
+        #     steamtinkerlaunch
+        #     ;
+        # };
       };
-      gamemode = {
-        enable = true;
-        enableRenice = true;
-        settings.custom.start = "${lib.getExe pkgsUnstable.libnotify} 'GameMode started'";
-        settings.custom.end = "${lib.getExe pkgsUnstable.libnotify} 'GameMode ended'";
-      };
-      gamescope.enable = true;
-      gamescope.capSysNice = true;
+      # gamemode = {
+      #   enable = true;
+      #   enableRenice = true;
+      #   settings.custom.start = "${lib.getExe pkgsUnstable.libnotify} 'GameMode started'";
+      #   settings.custom.end = "${lib.getExe pkgsUnstable.libnotify} 'GameMode ended'";
+      # };
+      # gamescope.enable = true;
+      # gamescope.capSysNice = true;
     };
   };
 }
