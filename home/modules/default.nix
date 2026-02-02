6f0e2118d100d5ff72a6dd84e30eaf5e0c6a8445
sm-idk <43745781+sm-idk@users.noreply.github.com>
2026-02-02 17:51:53 +0100

    Syntax improvements

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -1,16 +1,13 @@
-_: {
+{
   imports = [
     ./chromium
-    # ./waybar
     ./git.nix
     ./keepassxc.nix
-    # ./mako.nix
     ./niri.nix
     ./noctalia.nix
     ./spicetify.nix
     ./stylix.nix
     ./vicinae.nix
     ./zed.nix
-    # ./zen.nix
   ];
 }
--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,4 +1,4 @@
-_: {
+{
   imports = [
     ./audio.nix
     ./gnome.nix
