0b85ce121d8c7207144ef1952624077298dd1477
sm-idk <43745781+sm-idk@users.noreply.github.com>
2026-05-26 23:49:55 +0200

    Update nix.nix

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -27,12 +27,12 @@
 
       extra-substituters = [
         "https://vicinae.cachix.org"
-
+        "https://noctalia.cachix.org"
         "https://nixos-apple-silicon.cachix.org"
       ];
       extra-trusted-public-keys = [
         "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
-
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
       ];
 
