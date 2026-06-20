d227f8ce5b2b0553e270ace559cc1c7a2b444b2b
sm-idk <43745781+sm-idk@users.noreply.github.com>
2026-06-21 00:14:23 +0200

    start keepassxc with graphical session

--- home/modules/keepassxc.nix
+++ home/modules/keepassxc.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   home.packages = builtins.attrValues {
     inherit (pkgs)
@@ -17,5 +17,18 @@
       TrayIconAppearance = "monochrome-light";
     };
     SecretServiceIntegration.Enabled = true;
+  };
+
+  # Start KeePassXC with the graphical session so Secret Service and browser
+  # integration are available immediately after unlocking your database.
+  systemd.user.services.keepassxc = {
+    Unit = {
+      Description = "KeePassXC password manager";
+      PartOf = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${lib.getExe pkgs.keepassxc} --minimized";
+    };
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 }
