7f9a79b74c296656035a34703f4b2ef660fbd61d
sm-idk <43745781+sm-idk@users.noreply.github.com>
2026-06-21 00:30:58 +0200

    remove dead steam comments

--- modules/core/steam.nix
+++ modules/core/steam.nix
@@ -10,69 +10,12 @@
   config = lib.mkIf config.nixos.steam.enable {
     hardware.steam-hardware.enable = true;
 
-    # nixpkgs.overlays = [
-    #   (_: super: {
-    #     steam = super.steam.override {
-    #       extraPkgs =
-    #         steamSuper:
-    #         (builtins.attrValues {
-    #           inherit (steamSuper)
-    #             curl
-    #             mesa-demos
-    #             imagemagick
-    #             keyutils
-    #             mangohud
-    #             source-han-sans
-    #             steamtinkerlaunch # just in case compattools doesn't works
-    #             vkbasalt
-    #             vulkan-validation-layers
-    #             wqy_zenhei
-    #             yad
-    #             ;
-    #           inherit (pkgs.unstable)
-    #             libgdiplus
-    #             libkrb5
-    #             libpng
-    #             libpulseaudio
-    #             libvorbis
-    #             ;
-    #           inherit (pkgs.unstable)
-    #             vulkan-caps-viewer
-    #             vulkan-extension-layer
-    #             vulkan-headers
-    #             vulkan-tools
-    #             ;
-    #           inherit (steamSuper.xorg)
-    #             libXcursor
-    #             libXi
-    #             libXinerama
-    #             libXScrnSaver
-    #             xhost
-    #             ;
-    #           inherit (steamSuper.stdenv.cc.cc) lib;
-    #         });
-    #     };
-    #     # for people that want non official bottles
-    #     bottles = super.bottles.override { removeWarningPopup = true; };
-    #   })
-    # ];
-
     programs = {
       steam = {
         enable = true;
         protontricks.enable = true;
         remotePlay.openFirewall = true;
         localNetworkGameTransfers.openFirewall = true;
-        # https://github.com/NixOS/nixpkgs/blob/3730d8a308f94996a9ba7c7138ede69c1b9ac4ae/nixos/modules/programs/steam.nix#L12C3-L12C92
-        # steam added a proper search for steamcompattools in your ~/.steam so variable should not be needed anymore
-        # don't forget to periodically change steam's compatibility layer
-        #
-        # extraCompatPackages = builtins.attrValues {
-        #   inherit (pkgs.unstable)
-        #     # proton-ge-bin
-        #     steamtinkerlaunch
-        #     ;
-        # };
       };
       gamemode = {
         enable = true;
